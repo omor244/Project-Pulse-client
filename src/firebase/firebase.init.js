a8bc700c48905723a2ba9006af92639300e68138
Abdur55 <mrrahim972@gmail.com>
2025-12-29 07:55:13 +0600

    added env file

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBVDxchwcLVl3_spYNQFyFdHR6t43IkR5E",
-    authDomain: "project-pluse.firebaseapp.com",
-    projectId: "project-pluse",
-    storageBucket: "project-pluse.firebasestorage.app",
-    messagingSenderId: "393242981702",
-    appId: "1:393242981702:web:eeb41b96ac0b9736f82147"
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId,
 };
 
 // Initialize Firebase
